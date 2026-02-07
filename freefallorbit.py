68eb5a2c81a7620528bc51974272c297e8641a31
pranesh <pd722@ic.ac.uk>
2026-02-07 19:50:14 +0000

    wokring orbit

--- freefallorbit.py
+++ freefallorbit.py
@@ -17,7 +17,7 @@ class object:
         total_mass = self.mass + other.mass
         return (self.mass * self.position + other.mass * other.position) / total_mass
      
-    scale = 250 / 1.496e11 # 1 AU = 1.496e11 m, scale to fit in window
+    scale = 250 / (5 * 1.496e11) # 1 AU = 1.496e11 m, scale to fit in window
 
     def __init__(self, mass, radius, position = np.array([0, 0]), velocity = np.array([0, 0]), colour = (255, 255, 255)):
         self.mass = mass
@@ -30,30 +30,28 @@ class object:
     def draw(self, window):
         pygame.draw.circle(window, self.colour, (int(self.position[0]), int(self.position[1])), int(self.radius))
 
-    
-    
     def gravitational_acceleration(self, other):
         
         r_vector = (other.position - self.position) / self.scale # convert back to meters
         r_magnitude = np.linalg.norm(r_vector)
 
-        # if r_magnitude == 0:
-        #     return np.array([0, 0]) # avoid division by zero
+        if r_magnitude == 0:
+            return np.array([0, 0]) # avoid division by zero
         
         force_magnitude = G * self.mass * other.mass / r_magnitude**2
         force_direction = r_vector / r_magnitude
 
         return force_magnitude * force_direction / self.mass # F = ma => a = F/m
     
-    def update_position(self, other, dt = 60*60*24):
+    def update_position(self, other, dt = 60*60*24*12):
         self.acceleration = self.gravitational_acceleration(other)
-        self.position += self.velocity * dt
-        self.velocity += self.acceleration * dt
+        self.velocity += np.array(self.acceleration * dt, dtype = int)
+        self.position += np.array(self.velocity * dt * self.scale, dtype = int) # convert back to pixels
     
 #-=-=-=-=-=-=-=-=-main program-=-=-=-=-=-=-=-=-=-=-=-=-
 
-earth = object(5.972e24, 20, np.array([1.496e11, 0]))
 sun = object(1.989e30, 30, np.array([0, 0]))
+jupiter = object(1.898e27, 25, np.array([7.785e11, 0]), np.array([0, 13070]))
 
 def main():
     run = True
@@ -66,12 +64,15 @@ def main():
         window.fill((0, 0, 0))
         
         
-        
-        earth.update_position(sun)
-        sun.update_position(earth)
-        earth.draw(window)
+        sun.update_position(jupiter)
+        jupiter.update_position(sun)
+
+        jupiter.draw(window)
         sun.draw(window)
 
+        plt.plot(jupiter.position[0], jupiter.position[1], 'o', color = 'blue')
+        plt.plot(sun.position[0], sun.position[1], 'o', color = 'yellow')
+        
 
         pygame.display.flip()
 
@@ -79,8 +80,9 @@ def main():
 
 main()
 #-=-=-=-=-=-=-printing parameters-=-=-=-=-=-=-=-=-=-=-=-=-
-
-print(earth.gravitational_acceleration(sun))
-print(sun.gravitational_acceleration(earth))
-print('COM', earth.centreofmass(sun))
-print('COM', sun.centreofmass(earth))
+    
+print("Earth's gravitational acceleration towards Sun:", jupiter.gravitational_acceleration(sun))
+print("Sun's gravitational acceleration towards Earth:", sun.gravitational_acceleration(jupiter))
+print('COM', jupiter.centreofmass(sun))
+print('COM', sun.centreofmass(jupiter))
+plt.show()
